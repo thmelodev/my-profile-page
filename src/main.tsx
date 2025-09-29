94d22c401a7ffd02851fb6e7426d5b4c5b86f536
thiago.pereira <thiago.pereira@quero-quero.com.br>
2025-09-29 18:06:32 -0300

    Revert "feat: adicionando vercel insights"
    This reverts commit eba7d399f236f55878ce41358e9454312e94e8a9.

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Home } from './pages/Home.tsx'
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Home />
-    <SpeedInsights />
   </StrictMode>,
 )
